c052a032a1333c45254021b3e5a7171c03caa717
Sagnik Pramanik <sagnikpramanik95@gmail.com>
2023-06-29 17:52:00 +0530

    basic winning logic

--- Client/script.js
+++ Client/script.js
@@ -1,17 +1,24 @@
-const playerName = window.prompt('Enter Your name:');
-
-
+input = window.prompt('Enter Your name:');
+while(input ==='' || input == null)
+{
+    input=window.prompt("Enter your Name");
+}
+const playerName = input;
+console.log("Name is" + playerName);
 const socket = io("ws://localhost:5000");
 socket.emit("player-joined",playerName, () => {
     console.log("Informed the server that a player has joined");
 });
-var sym
+var sym;
 socket.on("player-approved", (playersSymbol) =>{
     this.sym=playersSymbol;
     console.log("you have been approved to play with symbol "+ sym);
 });
 socket.on("room-full",()=>{
     console.log("sorry, you are not allowed to play");
+});
+socket.on("message",(msg)=>{
+    console.log("msg received" + msg);
 });
 socket.on("board-update", (updatedBoard) => {
     console.log("Updated board received");
@@ -32,25 +39,13 @@ let bMatrix = [[document.getElementById("b00"),document.getElementById("b01"),do
 let board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
 function clicked(a,b)
 {
-    if(board[a][b] != ' ')
-    {
-        invalidMove();
-    }
-    else
-    {
-        board[a][b]=sym;
-        changeSymbol();
-        updateBoard();
-        checkStatus();
         socket.emit('tile-click',a,b,sym,()=>{
             console.log("emitted coordinates: " + a + b);
         })
-       
-    }
 }
 function resetButtonClicked()
 {
-    location.reload();
+    
 }
 function updateBoard()
 {
@@ -59,80 +54,6 @@ function updateBoard()
         for(let j=0;j<3;j++)
         {
             bMatrix[i][j].value=board[i][j];
-        }
-    }
-}
-function checkStatus()
-{
-    for (let i = 0; i < 3; i++) 
-    {
-        if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][2]!=' ') 
-        {
-            gameWon(board[i][0]);
-        }
-    }
-    for (let j = 0; j < 3; j++) 
-    {
-        if (board[0][j] === board[1][j] && board[1][j] === board[2][j] && board[2][j]!=' ') 
-        {
-            gameWon(board[0][j]);   
-        }
-    }
-    if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[2][2]!=' ') 
-    {
-        gameWon(board[0][0]);
-    }
-    if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[2][0]!=' ') 
-    {
-    gameWon(board[0][2]);
-    }
-    if(isDraw())
-        matchDraw();
-}
-function isDraw()
-{
-    for(let i=0;i<3;i++)
-    {
-        for(let j=0;j<3;j++)
-        {
-            if(board[i][j]==' ')
-                return false;
-        }
-    }
-    return true;
-}
-function matchDraw()
-{
-    disableButtons();
-    document.getElementById("msgbox").append("Draw ");
-}
-function changeSymbol()
-{
-    if(sym=='X')
-    {
-        sym='O';
-    }else{
-        sym='X';
-    }
-}
-function gameWon(s)
-{
-    document.getElementById("msgbox").append("Won by "+s+"")
-
-    msgbox.style.opacity=1;
-    disableButtons();
-}
-function invalidMove()
-{
-    window.alert("Invalid Move");
-}
-function disableButtons()
-{
-    for(let i=0;i<3;i++)
-    {
-        for(let j=0;j<3;j++)
-        {
-            bMatrix[i][j].disabled=true;
         }
     }
 }
--- Server/server.js
+++ Server/server.js
@@ -8,74 +8,45 @@ const io = new Server(server, { cors: {origin: "*",}, });
 var playersid=[];
 var players=[];
 const symbols=["X","O"];
-let board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
+var turn="X";
+var board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
 app.use( cors( {origin: "*",} ) );
 app.get("/", function (req, res) {res.sendFile(__dirname+"/index.html");});
-
 io.on("connection", (socket) => {
-
-    //socket.io work
+  console.log("inside");
     socket.on("player-joined", (playerName) => {
        console.log("new player " + playerName+ " wants to join");
       if(!(playerName in players) && players.length<2)
       { 
         players.push(playerName);
+        playersid.push(socket.id);
         socket.emit("player-approved",symbols[players.length-1]);
+        console.log("now playing\n");
         console.log(players);
+        console.log(playersid);
       }else{
         console.log("player "+ playerName + " was rejected");
         socket.emit("room-full");
       }
-    })
-    socket.on("entered-room", (roomid) => {
-      playersid.push(socket.id);
-      console.log("player with id " + playersid+ "entered the room" + playersid.length);
-      /*
-      let room = roomManager.getRoom(roomid);
-      if(!room) {
-        //if room does not exist, create a new room
-        roomManager.createRoom(roomid);
-        room = roomManager.getRoom(roomid);
-      }
-      let playerData = createPlayer(socket.id, shape);
-      room.players.push(playerData);
-      socket.roomId = room.id;
-      console.log(`User with socket id ${socket.id} connected to room ${socket.roomId}`);
-      //emit the info to all clients
-      */
-      socket.emit("entered-room", roomid,()=>{
-        console.log("player entered, data emitted");
-      });
     });
     socket.on("tile-click", (a,b,sym) => {
       console.log("coordinates received "+sym+": ("+a+","+b+")");
-      if(board[a][b] != ' ')
-      {
-        console.log("Invalid move detected")
-        socket.emit("message","invalid move");
-      }
-      else
+      if(board[a][b] === ' ' && turn ===sym && players.length == 2)
       {
         console.log("valid move");
         board[a][b]=sym;
         io.emit('board-update',board);
+        checkStatus();
+        invertTurn();
       }
-
-
-/*
-      //if (roomManager.isRoomPlaying(socket.roomId)==false) { // match not in progress, this is the first move
-        room.tiles[coordinates] = roomManager.getPlayer(room, socket.id).shape;
-        room.nextPlayer = socket.id;//emit to all clients
-        io.emit("tile-click" + room.id, room);
-        console.log("tile clicked");
-        console.log(coordinates);
-        return;
-      //}
-      if (room.nextPlayer == socket.id || room.tiles[coordinates]) // either not this players turn or tile already filled, invalid move
-        return;
-      room.tiles[coordinates] = roomManager.getPlayer(room, socket.id).shape;
-      room.nextPlayer = socket.id;
-      io.emit("tile-click" + room.id, room); // emit to all clients*/
+      else 
+      {
+        console.log("Invalid move detected");
+        if (turn != sym)
+          socket.emit("message","not your turn");
+        else
+          socket.emit("message","wrong place");
+        }
     });
 
     socket.on("reset-game", (roomid) => {
@@ -83,7 +54,6 @@ io.on("connection", (socket) => {
       io.emit("reset-game" + roomid, roomid);
       io.emit("game-update" + roomid, roomManager.getRoom(roomid));
     });
-
     socket.on("get-room", (roomId) => {
       let room = roomManager.getRoom(roomId);
       socket.emit("get-room" + roomId, room);
@@ -91,23 +61,16 @@ io.on("connection", (socket) => {
     socket.on("create-room", (roomId) => {
       roomManager.createRoom(roomId);
     });
-    socket.on("disconnect", () => {
-      let room = roomManager.getRoom(socket.roomId);
-      if (!room) return; // room not made
-      for (let i = 0; i < room.players.length; i++) {
-        if (room.players[i].id === socket.id) {
-          room.players.splice(i, 1); // splice is used to remove the array element
-          io.emit("user-disconnected" + socket.roomId);
-          if (roomManager.alreadyPlaying(socket.roomId)) {
-            roomManager.resetGame(socket.roomId);
-            io.emit("reset-game" + socket.roomId, socket.roomId);
-            io.emit("game-update" + socket.roomId, roomManager.getRoom(socket.roomId));
-          }
-        }
-      }
-      console.log(`User ${socket.id} disconnected from room ${socket.roomId}`);
-    });
-  });
+    socket.on("disconnect", () =>
+    {
+      io.emit("disconnected",getNameFromId(socket.id));
+      console.log(`User ${socket.id} disconnected from the room`);
+      playersid.splice(playersid.indexOf(socket.id),1);
+      players.splice(playersid.indexOf(socket.id),1);
+      console.log(players);
+      console.log(playersid);
+     });
+});
 let PORT = 5000;
 
 server.listen(PORT, () => {
@@ -155,4 +118,30 @@ function isDraw()
 function gameWon(s)
 {
   console.log("game won by " + s);
+}
+function matchDraw()
+{
+    disableButtons();
+    document.getElementById("msgbox").append("Draw ");
+}
+function invertTurn()
+{
+  if(turn==="X")
+  {
+    turn="O";
+  }
+  else
+  {
+    turn="X";
+  }
+}
+function getNameFromId(id)
+{
+  return players[playersid.indexOf(id)];
+}
+function reset()
+{
+  board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
+  playersid=[];
+  players=[];
 }
